a1e0ba10e13136dc3272d568e3f2a647fd8ceaf5
Gabriel Henry Friederichs <ghfriederich@tux-150.cae.wisc.edu>
2022-03-26 14:56:23 -0500

    Updated release() and readBenchmark()

--- ece556.cpp
+++ ece556.cpp
@@ -1,8 +1,12 @@
 // ECE556 - Copyright 2014 University of Wisconsin-Madison.  All Rights Reserved.
 
+#include <string>
 #include <iostream>
 #include <fstream>
+#include <bits/stdc++.h>
 #include "ece556.h"
+
+using namespace std;
 
 // Reference for routingInst struct
 //  typedef struct
@@ -23,11 +27,36 @@
 
 int readBenchmark(const char *fileName, routingInst *rst){
   /*********** TO BE FILLED BY YOU **********/
+  /*
   std::ofstream myfile;
   myfile.open(fileName);
   myfile << "readBenchmark file\n";
   myfile.close();
+  */
+
+  // open read file
+  ifstream myfile;
+  myfile.open(fileName, ios::in);
+
+  // ensure file is open
+  if (!myfile.is_open()) return 0;
+
+  // read x and y dimensions of global routing grid
+  string line;
+  getline(myfile, line); // read line
+  stringstream linestream(line);
+  string item;
+  getline(linestream, item, ' '); // token 0
+
+  getline(linestream, item, ' '); // token 1
+  rst->gx = stoi(item);
+  getline(linestream, item, ' '); // token 2
+  rst->gy = stoi(item);
+
+  //
   
+  // clean up and return
+  myfile.close();
   return 1;
 }
 
@@ -39,7 +68,7 @@ int solveRouting(routingInst *rst){
 
 int writeOutput(const char *outRouteFile, routingInst *rst){
   /*********** TO BE FILLED BY YOU **********/
-  std::ofstream myfile;
+  ofstream myfile;
   myfile.open(outRouteFile);
   myfile << "writeOutput file\n";
   myfile.close();
@@ -48,16 +77,41 @@ int writeOutput(const char *outRouteFile, routingInst *rst){
 }
 
 
+/**
+ * Need to recursively delete all memory allocations from
+ * bottom to top (e.g., starting from segments then routes
+ * then individual fields within a net struct, then the
+ * nets, then the fields in a routing instance, and finally
+ * the routing instance)
+ */
+
+// NOTE: do these need to be in for loops?
 int release(routingInst *rst){
   /*********** TO BE FILLED BY YOU **********/
   if (!rst) return 0; // failure if rst is NULL
 
-  // free all memory structures in rst
+  // release all edges (???)
+  free(rst->nets->nroute.segments->edges);
+
+  // release all segments
+  free(rst->nets->nroute.segments);
+  
+  // release all routes (???)
+  //free(rst->nets->nroute);
+
+  // release all fields within net struct
+  free(rst->nets->pins);
+
+  // release all nets
   free(rst->nets);
+  
+  // release fields within routing instance
   free(rst->edgeCaps);
   free(rst->edgeUtils);
-  // free rst memory structure
+  
+  // release the routing instance
   free(rst);
+
   return 1; // success!
 }
   
