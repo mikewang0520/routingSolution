36c357c88c98b31aa43a0081e8fa65bf58f42c4f
mikewang0520 <85894910+mikewang0520@users.noreply.github.com>
2022-03-26 14:48:56 -0500

    Updated ece556.cpp

--- ece556.cpp
+++ ece556.cpp
@@ -1,30 +1,50 @@
 // ECE556 - Copyright 2014 University of Wisconsin-Madison.  All Rights Reserved.
-
+#include <fstream>
 #include "ece556.h"
 
-int readBenchmark(const char *fileName, routingInst *rst){
-  /*********** TO BE FILLED BY YOU **********/  
-
+// Reads in the benchmark file and initializes the routing instance
+// This function needs to populate some fields in the routingInst structure
+int readBenchmark(const char *fileName, routingInst *rst)
+{
+  /*********** TO BE FILLED BY YOU **********/
+  //declare file stream variable
+  ofstream fileIn;
+  //open the input file
+  fileIn.open(fileName);
+  //wirte to the input file
+  fileOut << ;
+  //close the input file
+  fileIn.close(); 
   return 1;
 }
 
-int solveRouting(routingInst *rst){
+// This function creates a routing solution
+int solveRouting(routingInst *rst)
+{
   /*********** TO BE FILLED BY YOU **********/
 
   return 1;
 }
 
-int writeOutput(const char *outRouteFile, routingInst *rst){
+// Write the routing solution
+int writeOutput(const char *outRouteFile, routingInst *rst)
+{
   /*********** TO BE FILLED BY YOU **********/
-
+  //declare file stream variable
+  ofstream fileOut;
+  //open the output file
+  fileOut.open(outRouteFile);
+  //wirte to the output file
+  fileOut << "";
+  //close the output file
+  fileOut.close();
   return 1;
 }
 
-
-int release(routingInst *rst){
+// Release memory used
+int release(routingInst *rst)
+{
   /*********** TO BE FILLED BY YOU **********/
-
+  free(rst);
   return 1;
 }
-  
-
