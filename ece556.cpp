5a6977998cfe2bf6c06613973e282407dc62833a
gfrieder1 <gfrieder101@gmail.com>
2022-03-28 18:35:23 -0500

    Update ece556.cpp

--- ece556.cpp
+++ ece556.cpp
@@ -1,20 +1,116 @@
 // ECE556 - Copyright 2014 University of Wisconsin-Madison.  All Rights Reserved.
+
+#include <string>
+#include <iostream>
 #include <fstream>
+#include <bits/stdc++.h>
 #include "ece556.h"
 
-// Reads in the benchmark file and initializes the routing instance
-// This function needs to populate some fields in the routingInst structure
-int readBenchmark(const char *fileName, routingInst *rst)
-{
+using namespace std;
+
+// Reference for routingInst struct
+//  typedef struct
+  //  {
+    //   int gx ;             /* x dimension of the global routing grid */
+    //   int gy ;             /* y dimension of the global routing grid */
+
+    //   int cap ;
+
+    //   int numNets ;        /* number of nets */
+    //   net *nets ;          /* array of nets */
+
+    //   int numEdges ;       /* number of edges of the grid */
+    //   int *edgeCaps;       /* array of the actual edge capacities after considering for blockages */
+    //   int *edgeUtils;      /* array of edge utilizations */
+
+    //  } routingInst ;
+
+int readBenchmark(const char *fileName, routingInst *rst){
   /*********** TO BE FILLED BY YOU **********/
-  //declare file stream variable
-  ofstream fileIn;
-  //open the input file
-  fileIn.open(fileName);
-  //wirte to the input file
-  fileOut << ;
-  //close the input file
-  fileIn.close(); 
+  /*
+  std::ofstream myfile;
+  myfile.open(fileName);
+  myfile << "readBenchmark file\n";
+  myfile.close();
+  */
+
+  // open read file
+  ifstream myfile;
+  myfile.open(fileName, ios::in);
+
+  // ensure file is open
+  if (!myfile.is_open()) return 0;
+
+  // read x and y dimensions of global routing grid
+  string line;
+  getline(myfile, line); // read line
+  stringstream linestream(line); // define linestream
+  string item; // define
+  getline(linestream, item, ' '); // token 0 ("grid")
+  getline(linestream, item, ' '); // token 1 (gx)
+  rst->gx = stoi(item);
+  // NOTE: THIS MIGHT NOT WORK BECAUSE THE DELIMITER SHOULD BE \n???
+  getline(linestream, item, ' '); // token 2 (gy)
+  rst->gy = stoi(item);
+  linestream.str("");
+  linestream.clear();
+
+  // read capacity number
+  getline(myfile, line); // read line
+  linestream.str(line);
+  getline(linestream, item, ' '); // token 0 ("capacity")
+  getline(linestream, item, ' '); // token 1 (cap)
+  rst->cap = stoi(item);
+  linestream.str("");
+  linestream.clear();
+
+  // read numNets
+  getline(myfile, line); // read line
+  linestream.str(line);
+  getline(linestream, item, ' '); // token 0 ("num")
+  getline(linestream, item, ' '); // token 1 ("net")
+  getline(linestream, item, ' '); // token 2 (numNets)
+  rst->numNets = stoi(item);
+  linestream.str("");
+  linestream.clear();
+
+  // allocate space for nets
+  rst->nets = (net*) malloc(rst->numNets * sizeof(net));
+
+  // read all pins of all nets (nested for loop)
+  for (int i = 0; i < rst->numNets; ++i) {
+    // iterate over all nets
+    getline(myfile, line); // read line
+    linestream.str(line);
+    getline(linestream, item, ' '); // token 0 (netName (e.g. "n0"))
+    int netid = stoi(item.substr(1)); // extracts number from "n0"
+    rst->nets[i].id = netid;
+    getline(linestream, item, ' '); // token 1 (numPins)
+    int numPins = stoi(item);
+    rst->nets[i].numPins = numPins;
+    linestream.str("");
+    linestream.clear();
+
+    // allocated per-net space for pins
+    rst->nets[i].pins = (point*) malloc(rst->nets[i].numPins * sizeof(point));
+
+    for (int j = 0; j < rst->nets[i].numPins; ++j) {
+      // iterate over all pins within a net
+      getline(myfile, line); // read line
+      linestream.str(line);
+      getline(linestream, item, ' '); // token 0 (x)
+      rst->nets[i].pins[j].x = stoi(item);
+      getline(linestream, item, '\t'); // token 1 (y)
+      rst->nets[i].pins[j].y = stoi(item);
+      linestream.str("");
+      linestream.clear();
+    }
+  }
+
+  // BLOCKAGES????
+
+  // clean up and return
+  //myfile.close();
   return 1;
 }
 
@@ -61,28 +157,54 @@ int writeOutput(const char *outRouteFile, routingInst *rst)
 }
 
 // Release memory used
-int release(routingInst *rst)
-{
+// NOTE: do these need to be in for loops?
+int release(routingInst *rst){
   /*********** TO BE FILLED BY YOU **********/
-  // release segments
-  free(rst.nets.nroute.segments);
-  // release routes
-  free(rst.nets.nroute);
-  // release fields in the net structure
-  free(rst.nets.id);
-  free(rst.nets.numPins);
-  free(rst.nets.pins);
-  // release nets
-  free(rst.nets);
-  // release fields in the routing instance
-  free(rst.gx);
-  free(rst.gy);
-  free(rst.cap);
-  free(rst.numNets);
-  free(rst.numEdges);
-  free(rst.edgeCaps);
-  free(rst.edgeUtils);
-  // release routing instance itself
+  if (!rst) return 0; // failure if rst is NULL
+
+
+  // NOTE: THESE ALL CURRENTLY SEGFAULT RIGHT NOW
+
+
+  // release all edges (???)
+  if (rst->nets->nroute.segments->edges) // SEGFAULT OOPS LOL
+    free(rst->nets->nroute.segments->edges);
+  else
+    cout << "No edges??" << endl;
+
+  // release all segments
+  if (rst->nets->nroute.segments)
+    free(rst->nets->nroute.segments);
+  else
+    cout << "No segments??" << endl;
+
+  // release all routes (???)
+  //free(rst->nets->nroute);
+
+  // release all fields within net struct
+  if (rst->nets->pins)
+    free(rst->nets->pins);
+  else
+    cout << "No pins??" << endl;
+
+  // release all nets
+  if (rst->nets)
+    free(rst->nets);
+  else
+    cout << "No nets??" << endl;
+
+  // release fields within routing instance
+  if (rst->edgeCaps)
+    free(rst->edgeCaps);
+  else
+    cout << "No edgeCaps??" << endl;
+  if (rst->edgeUtils)
+    free(rst->edgeUtils);
+  else
+    cout << "No edgeUtils??" << endl;
+
+  // release the routing instance
   free(rst);
-  return 1;
+
+  return 1; // success!
 }
