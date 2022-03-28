fb7a347500d6909b9740b7be82f98360baea7e55
Gabriel Henry Friederichs <ghfriederich@tux-103.cae.wisc.edu>
2022-03-28 18:25:37 -0500

    readBenchmark net reading

--- ece556.cpp
+++ ece556.cpp
@@ -44,19 +44,73 @@ int readBenchmark(const char *fileName, routingInst *rst){
   // read x and y dimensions of global routing grid
   string line;
   getline(myfile, line); // read line
-  stringstream linestream(line);
-  string item;
-  getline(linestream, item, ' '); // token 0 (garbage)
-
-  getline(linestream, item, ' '); // token 1
+  stringstream linestream(line); // define linestream
+  string item; // define
+  getline(linestream, item, ' '); // token 0 ("grid")
+  getline(linestream, item, ' '); // token 1 (gx)
   rst->gx = stoi(item);
-  getline(linestream, item, ' '); // token 2
+  // NOTE: THIS MIGHT NOT WORK BECAUSE THE DELIMITER SHOULD BE \n???
+  getline(linestream, item, ' '); // token 2 (gy)
   rst->gy = stoi(item);
+  linestream.str("");
+  linestream.clear();
+  
+  // read capacity number
+  getline(myfile, line); // read line
+  linestream.str(line);
+  getline(linestream, item, ' '); // token 0 ("capacity")
+  getline(linestream, item, ' '); // token 1 (cap)
+  rst->cap = stoi(item);
+  linestream.str("");
+  linestream.clear();
 
-  //
+  // read numNets
+  getline(myfile, line); // read line
+  linestream.str(line);
+  getline(linestream, item, ' '); // token 0 ("num")
+  getline(linestream, item, ' '); // token 1 ("net")
+  getline(linestream, item, ' '); // token 2 (numNets)
+  rst->numNets = stoi(item);
+  linestream.str("");
+  linestream.clear();
+
+  // allocate space for nets
+  rst->nets = (net*) malloc(rst->numNets * sizeof(net));
+
+  // read all pins of all nets (nested for loop)
+  for (int i = 0; i < rst->numNets; ++i) {
+    // iterate over all nets
+    getline(myfile, line); // read line
+    linestream.str(line);
+    getline(linestream, item, ' '); // token 0 (netName (e.g. "n0"))
+    int netid = stoi(item.substr(1)); // extracts number from "n0"
+    rst->nets[i].id = netid;
+    getline(linestream, item, ' '); // token 1 (numPins)
+    int numPins = stoi(item);
+    rst->nets[i].numPins = numPins;
+    linestream.str("");
+    linestream.clear();
+
+    // allocated per-net space for pins
+    rst->nets[i].pins = (point*) malloc(rst->nets[i].numPins * sizeof(point));
+    
+    for (int j = 0; j < rst->nets[i].numPins; ++j) {
+      // iterate over all pins within a net
+      getline(myfile, line); // read line
+      linestream.str(line);
+      getline(linestream, item, ' '); // token 0 (x)
+      rst->nets[i].pins[j].x = stoi(item);
+      getline(linestream, item, '\t'); // token 1 (y)
+      rst->nets[i].pins[j].y = stoi(item);
+      linestream.str("");
+      linestream.clear();
+    }
+  }
+
+  // BLOCKAGES????
   
   // clean up and return
-  myfile.close();
+  //myfile.close();
   return 1;
 }
 
@@ -90,24 +144,46 @@ int release(routingInst *rst){
   /*********** TO BE FILLED BY YOU **********/
   if (!rst) return 0; // failure if rst is NULL
 
+
+  // NOTE: THESE ALL CURRENTLY SEGFAULT RIGHT NOW
+  
+  
   // release all edges (???)
-  free(rst->nets->nroute.segments->edges);
+  if (rst->nets->nroute.segments->edges) // SEGFAULT OOPS LOL
+    free(rst->nets->nroute.segments->edges);
+  else
+    cout << "No edges??" << endl;
 
   // release all segments
-  free(rst->nets->nroute.segments);
+  if (rst->nets->nroute.segments)
+    free(rst->nets->nroute.segments);
+  else
+    cout << "No segments??" << endl;
   
   // release all routes (???)
   //free(rst->nets->nroute);
 
   // release all fields within net struct
-  free(rst->nets->pins);
+  if (rst->nets->pins)
+    free(rst->nets->pins);
+  else
+    cout << "No pins??" << endl;
 
   // release all nets
-  free(rst->nets);
+  if (rst->nets)
+    free(rst->nets);
+  else
+    cout << "No nets??" << endl;
   
   // release fields within routing instance
-  free(rst->edgeCaps);
-  free(rst->edgeUtils);
+  if (rst->edgeCaps)
+    free(rst->edgeCaps);
+  else
+    cout << "No edgeCaps??" << endl;
+  if (rst->edgeUtils)
+    free(rst->edgeUtils);
+  else
+    cout << "No edgeUtils??" << endl;
   
   // release the routing instance
   free(rst);
